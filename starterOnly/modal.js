fb57db83895bfa9c612f4e62f6c227286a50cda4
boxtober <contact.asartwork@gmail.com>
2023-12-22 11:47:40 +0100

    :sparkles: feat: Add form validity functions

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,14 +1,10 @@
-function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
+const editNav = () => {
+  let nav = document.getElementById("myTopnav");
+  nav.className = (nav.className === "topnav") ? "topnav responsive" : "topnav";
+};
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
+const modalbg = document.getElementById("form");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const modalClose = document.querySelector(".close");
@@ -26,3 +22,59 @@ modalClose.addEventListener("click", closePls)
 function closePls() {
   modalbg.style.display = "none";
 }
+
+document.getElementById("form-register").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let checkFirstname = () => {
+
+    const firstName = document.getElementById("firstName");
+    const firstNameValue = firstName.value.trim();
+
+  }
+
+  function checkLastname() {
+
+    const userName = document.getElementById("userName");
+    const userNameValue = userName.value.trim();
+
+  }
+
+  function checkEmail() {
+    const emailElement = document.getElementById("email");
+    const emailValue = emailElement.value.trim();
+
+  }
+
+  function checkTournament() {
+
+    const eventInput = document.getElementById("eventsNbr");
+    const eventValue = parseInt(eventInput.value, 10);
+
+  }
+
+  function checkCity() {
+    const radios = document.querySelectorAll('input[name="location"]');
+
+  }
+
+  function checkBirthday() {
+    const birthdayElement = document.getElementById('birthday');
+    const birthdayValue = birthdayElement.value;
+
+  }
+
+  function checkTerms() {
+    const checkbox1 = document.getElementById("checkbox1");
+    const checkbox1Valid = checkbox1.checked;
+  }
+
+  checkTerms();
+  checkBirthday();
+  checkCity();
+  checkTournament();
+  checkEmail();
+  checkFirstname();
+  checkLastname();
+
+});
