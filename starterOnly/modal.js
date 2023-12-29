5d5ffe80062fe59dc04db1ff1a46eb4fd1814ead
boxtober <contact.asartwork@gmail.com>
2023-12-29 10:57:03 +0100

    :sparkles: feat: Add conditions and error messages

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -23,51 +23,167 @@ function closePls() {
   modalbg.style.display = "none";
 }
 
-document.getElementById("form-register").addEventListener("submit", (e) => {
+
+
+let checkFirstname = () => {
+
+  const firstName = document.getElementById("firstName");
+  const firstNameValue = firstName.value.trim();
+  let parentElement = firstName.parentNode;
+
+  const validCharacters = /^[a-zA-Z\-]+$/;
+
+  if (firstNameValue.length < 2) {
+
+    parentElement.setAttribute('data-error-visible', 'true');
+    parentElement.setAttribute('data-error', 'Veuillez entrer au minimum 2 caractères');
+
+  } else if (!validCharacters.test(firstNameValue)) {
+
+    parentElement.setAttribute('data-error-visible', 'true');
+    parentElement.setAttribute('data-error', 'Aucun chiffre autorisé');
+
+  } else {
+    parentElement.setAttribute('data-error-visible', 'false');
+    console.log(firstName.parentNode);
+  }
+}
+
+let checkLastname = () => {
+
+  const userName = document.getElementById("userName");
+  const userNameValue = userName.value.trim();
+  let parentElement = userName.parentNode;
+
+  const validCharacters = /^[a-zA-Z\-]+$/;
+
+  if (userNameValue.length < 2) {
+
+    parentElement.setAttribute('data-error-visible', 'true');
+    parentElement.setAttribute('data-error', 'Veuillez entrer au minimum 2 caractères');
+
+  } else if (!validCharacters.test(userNameValue)) {
+
+    parentElement.setAttribute('data-error-visible', 'true');
+    parentElement.setAttribute('data-error', 'Aucun chiffre autorisé');
+
+  } else {
+    parentElement.setAttribute('data-error-visible', 'false');
+    console.log(userName.parentNode);
+
+  }
+}
+let checkBirthday = () => {
+  const birthdayElement = document.getElementById('birthday');
+  const birthdayValue = birthdayElement.value;
+  const dateRegex = /^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$/;
+
+  let parentElement = birthdayElement.parentNode;
+  const birthDate = new Date(birthdayValue);
+  const currentYear = new Date().getFullYear();
+  const age = currentYear - birthDate.getFullYear();
+
+  if (!dateRegex.test(birthdayValue) && birthdayValue.trim() === '') {
+    console.log('trim test');
+    parentElement.setAttribute('data-error-visible', 'true');
+    parentElement.setAttribute('data-error', 'Veuillez saisir une DATE valide.');
+
+  } else if (isNaN(birthDate.getTime())) {
+    console.log('is nan');
+    parentElement.setAttribute('data-error-visible', 'true');
+    parentElement.setAttribute('data-error', 'La DATE n\'existe pas.');
+
+  } else if (age < 16) {
+    console.log('inférieur 16');
+    parentElement.setAttribute('data-error-visible', 'true');
+    parentElement.setAttribute('data-error', 'Vous devez avoir plus de 16 ans');
+
+  } else {
+    console.log('else');
+    parentElement.setAttribute('data-error-visible', 'false');
+  }
+}
+let checkEmail = () => {
+
+  const emailElement = document.getElementById("email");
+  const emailValue = emailElement.value.trim();
+  let parentElement = emailElement.parentNode;
+  const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  if (emailRegex.test(emailValue)) {
+    mailValid = true;
+    parentElement.setAttribute('data-error-visible', 'false');
+    console.log(emailElement.parentNode);
+
+  } else {
+    parentElement.setAttribute('data-error-visible', 'true');
+    parentElement.setAttribute('data-error', 'Email invalide !');
+
+  }
+}
+
+let checkTournament = () => {
+
+  const eventInput = document.getElementById("eventsNbr");
+  const eventValue = parseInt(eventInput.value, 10);
+  let parentElement = eventInput.parentNode;
+
+  if (eventValue > 0) {
+    parentElement.setAttribute('data-error-visible', 'false');
+
+  } else {
+    parentElement.setAttribute('data-error-visible', 'true');
+    parentElement.setAttribute('data-error', 'Veuillez saisir une valeur');
+
+  }
+}
+
+let checkCity = () => {
+
+  const radios = document.querySelectorAll('input[name="location"]');
+  let isValidOption = false;
+
+  radios.forEach(function (radio) {
+    if (radio.checked) {
+      isValidOption = true;
+    }
+  });
+
+  if (isValidOption) {
+    console.log(radios[0].parentNode);
+    let parentElement = radios[0].parentNode;
+
+    parentElement.setAttribute('data-error-visible', 'false');
+
+  } else {
+    let parentElement = radios[0].parentNode;
+    parentElement.setAttribute('data-error-visible', 'true');
+    parentElement.setAttribute('data-error', 'Sélectionnez au moins une option !');
+
+  }
+}
+
+
+
+let checkTerms = () => {
+  const checkbox1 = document.getElementById("checkbox1");
+  const checkbox1Valid = checkbox1.checked;
+  let parentElement = checkbox1.parentNode;
+  if (!checkbox1Valid) {
+    parentElement.setAttribute('data-error-visible', 'true');
+    parentElement.setAttribute('data-error', "Veuillez accepter les conditions d'utilisation");
+
+  } else {
+    parentElement.setAttribute('data-error-visible', 'false');
+    console.log(checkbox1.parentNode);
+
+  }
+}
+
+
+let registerForm = document.getElementById("form-register")
+registerForm.addEventListener("submit", (e) => {
   e.preventDefault();
-
-  let checkFirstname = () => {
-
-    const firstName = document.getElementById("firstName");
-    const firstNameValue = firstName.value.trim();
-
-  }
-
-  function checkLastname() {
-
-    const userName = document.getElementById("userName");
-    const userNameValue = userName.value.trim();
-
-  }
-
-  function checkEmail() {
-    const emailElement = document.getElementById("email");
-    const emailValue = emailElement.value.trim();
-
-  }
-
-  function checkTournament() {
-
-    const eventInput = document.getElementById("eventsNbr");
-    const eventValue = parseInt(eventInput.value, 10);
-
-  }
-
-  function checkCity() {
-    const radios = document.querySelectorAll('input[name="location"]');
-
-  }
-
-  function checkBirthday() {
-    const birthdayElement = document.getElementById('birthday');
-    const birthdayValue = birthdayElement.value;
-
-  }
-
-  function checkTerms() {
-    const checkbox1 = document.getElementById("checkbox1");
-    const checkbox1Valid = checkbox1.checked;
-  }
 
   checkTerms();
   checkBirthday();
@@ -78,3 +194,5 @@ document.getElementById("form-register").addEventListener("submit", (e) => {
   checkLastname();
 
 });
+
+validateForm();
